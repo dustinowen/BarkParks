e0ece237f4f53ac489cc85d2fa10c6167143991c
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-09-27 12:31:06 -0700

    Reset database; build user profile page with user reviews

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,9 +1,9 @@
 from django.forms import ModelForm
-from .models import Review
+from .models import Reviews
 
 class ReviewForm(ModelForm):
   class Meta:
-    model = Review
+    model = Reviews
     fields = ['date', 'rating', 'review']
 
 class UserForm(ModelForm):
