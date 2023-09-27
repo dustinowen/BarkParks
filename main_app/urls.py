4be999b6c48fb968be48fd3b3bd46234714422f6
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-09-27 11:37:10 -0700

    Build review functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,11 +3,19 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
     path('parks/', views.parks_index, name='parksindex'),
 
-    
+
     path('parks/<int:park_id>/', views.parks_detail, name="park_details"),
+
+    path('park/<int:park_id>/addreview/', views.add_review, name="addreview"),
+
+    path('park/<int:park_id>/postreview/', views.post_review, name="postreview"),
+
     path('user/', views.user_profile, name='user_profile'),
+
     path('user/favorites/', views.favorites, name='favorites'),
+
     path('map/', views.map, name='map'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import DogParks
+from .forms import ReviewForm
 
 
 def home(request):
@@ -12,7 +13,20 @@ def parks_index(request):
 
 def parks_detail(request, park_id):
     park = DogParks.objects.get(id=park_id)
-    return render(request, 'parks/details.html', { 'park': park })
+    return render(request, 'parks/details.html', { 'park': park})
+
+def add_review(request, park_id):
+    park= DogParks.objects.get(id=park_id)
+    review_form = ReviewForm()
+    return render(request, 'parks/addreview.html', {'park': park, 'review_form': review_form })
+
+def post_review(request, park_id):
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+        new_review = form.save(commit=False)
+        new_review.park_id = park_id
+        new_review.save()
+    return redirect('park_details', park_id=park_id)
 
 def map(request):
     return render(request, 'map/index.html')
