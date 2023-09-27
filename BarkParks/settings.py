9fbfc779c4cdca284b4c8bb97771e83fe4e955a4
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-09-27 09:36:47 -0700

    Building out reviews model 1:M

--- BarkParks/settings.py
+++ BarkParks/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'BarkParks.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'barkparks',
+        'NAME': 'bark_park_app',
     }
 }
 
--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+RATINGS = (
+    (5, '5-Stars'),
+    (4, '4-Stars'),
+    (3, '3-Stars'),
+    (2, '2-Stars'),
+    (1, '1-Star'),
+)
+
 # Create your models here.
 class DogParks(models.Model):
     name = models.CharField(max_length=50)
@@ -11,3 +19,14 @@ class DogParks(models.Model):
     def __str__(self):
         return self.name
     
+class Review(models.Model):
+    date = models.DateField()
+    rating = models.IntegerField(
+        choices=RATINGS, 
+        default=RATINGS[0][0]
+    )
+    park = models.ForeignKey(DogParks, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_rating_display()}"
+
