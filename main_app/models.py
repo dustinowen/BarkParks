ca3d2a44a91f9c1a10f93bc41c7bcfd3378e1b1a
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-09-26 20:26:55 -0700

    Build parks model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Parks(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    hours = models.CharField(max_length=100)
+    LAT = models.DecimalField(max_digits=9, decimal_places=6)
+    LON = models.DecimalField(max_digits=9, decimal_places=6)
+
+    def __str__(self):
+        return self.name
+    
+    
