26d1ae0bce558668556e83af4b36186a2761f692
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-09-30 09:23:21 -0700

    Complete map.js for map build out with geocode

--- main_app/static/js/map.js
+++ main_app/static/js/map.js
@@ -1,3 +1,5 @@
+import './geocode.js'
+
 function initMap() {
 
     map = new google.maps.Map(document.getElementById("map"), {
@@ -8,7 +10,7 @@ function initMap() {
 }
   
 function getResults(location) {
-    let userLocation = new google.maps.LatLng(location.lat, location.long)
+    let userLocation = new google.maps.LatLng(latitude, longitude)
     let request = {
         location: userLocation,
         radius: '10000',
@@ -19,4 +21,49 @@ function getResults(location) {
     service.nearbySearch(request, callback)
 }
 
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        cardContainer.innHTML = ""
+        for (let i = 0; i < results.length; i++) {
+            let place = results[i]
+            console.log(results)
+            let content = '<div> < h5 > ${ place.name }</h5> <p>${place.vicinity}</p></div>'
 
+            //build out display area for results
+            const res_box = document.createElement("div")
+            const name = document.createElement("h4")
+            const address = document.createElement("p")
+            
+            name.append(place.name)
+            address.append(place.vicinity)
+            
+            //disperse results over created areas
+            res_box.setAttribute("class", "card")
+            cardContainer.append(res_box)
+            res_box.append(name)
+            res_box.append(address)
+
+            //created custom marker with dog icon
+            let marker = new google.maps.Marker({
+                position: place.geometry.location,
+                map: map,
+                title: place.name,
+                label: "🐶",
+            })
+
+            let info_box = new google.maps.InfoWindow({ content: content })
+
+            groupInfoWindow(marker, map, info_box, content)
+            marker.setMap(map)
+        }
+    }
+
+    //created event listener for marker clicks to display results information
+    function groupInfoWindow(marker, map, info_box, html) {
+        marker.addListener('click', function () {
+            info_box.setContent(html)
+            info_box.open(map, this)
+        })
+    }
+
+}
