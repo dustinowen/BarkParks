082c328200c7eacebf1fc434bbaa683b0184ee99
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-09-29 22:24:06 -0700

    Reroute logout to homepage

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    
+
     path('', views.home, name='home'),
 
     path('parks/', views.parks_index, name='parksindex'),
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from .models import DogParks, Reviews
 from .forms import ReviewForm, ImageForm
 
