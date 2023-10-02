f3973d262720170c59706df8f5f61d5a7f5a6e72
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-10-02 06:22:12 -0700

    Cleanup settings.py

--- BarkParks/settings.py
+++ BarkParks/settings.py
@@ -24,8 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config('DJANGO_SECRET')
-
 
 SECRET_KEY = os.environ['DJANGO_SECRET']
 print("DJANGO_SECRET:", SECRET_KEY)
