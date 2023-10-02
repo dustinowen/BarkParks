f4e2fcfb4f92d811cd7b77af64adc517d30f43ba
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-10-02 06:19:05 -0700

    Correct error with environ

--- BarkParks/settings.py
+++ BarkParks/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['DJANGO_SECRET']
 # SECRET_KEY = config('DJANGO_SECRET')
-SECRET_KEY = os.getenv['DJANGO_SECRET']
 
+
+SECRET_KEY = os.environ['DJANGO_SECRET']
 print("DJANGO_SECRET:", SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
