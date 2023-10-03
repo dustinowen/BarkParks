a37347fca1525a47e51134b70eba0d236163da64
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-10-02 23:55:52 -0700

    Debug maps

--- BarkParks/settings.py
+++ BarkParks/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = os.environ['DJANGO_SECRET']
-GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
+GOOGLE_API_KEY = os.environ.get['GOOGLE_API_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
