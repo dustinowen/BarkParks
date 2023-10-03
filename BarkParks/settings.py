33f7ac6041f547524673744a12ab7cb20bb7b971
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-10-03 00:10:36 -0700

    Update KEY

--- BarkParks/settings.py
+++ BarkParks/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
 SECRET_KEY = os.environ['DJANGO_SECRET']
 GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
 
